953c4ddec32e0be15bf28aba768353cb9cf23157
jkcraftdom <jkcraftdom@gmail.com>
2022-07-19 16:29:11 -0500

    refactoring the user registry

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import {useState} from 'react'
+import {styleForm} from '../components/styles'
+import {
+  Card, 
+  ImageUser,
+  Label,
+  Button,
+  MessageError,
+  Input  
+} from '../components/form'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { loginUser, formValues, setToken } from '../services/auth'
+import { useNavigate } from 'react-router-dom'
+import useTimer from '../hooks/useTimer'
 
 const login = () => {
+  const [isLoading, setLoading] = useState<boolean>(false)
+  const {value: showMessage, initTimer} = useTimer(2)
+  const {register, handleSubmit} = useForm<formValues>()
+  let navigate = useNavigate()
+
+  const onSubmit:SubmitHandler<formValues> =  async (data) => { 
+
+    try{
+      setLoading(true)
+        let res =  await loginUser({ 
+        password: data.password, 
+        username: data.username
+      })     
+      setToken(res.data) 
+      setToken({"token":"token"})
+      navigate("/panel-user", {replace:true})
+
+    }catch(error){
+      console.log(error)
+      initTimer()
+    }finally{
+      setLoading(false)
+    }
+
+  }
+
   return (
-    <div>login</div>
+    <Card>
+      <ImageUser />
+      {showMessage && <MessageError>Datos incorrectos</MessageError>}
+      <form action=""  style={styleForm} onSubmit={handleSubmit(onSubmit)}>
+        <Label >Usuario</Label>
+        <Input {...register('username')}/>
+        <Label >Contraseña</Label>
+        <Input type="password" {...register('password')}/>
+        <Button >{isLoading? "Cargando ...":"Iniciar sesión"}</Button>
+      </form>
+    </Card>
   )
 }
 
---
+++ src/schemas/auth/index.ts
@@ -0,0 +1,7 @@
+import * as yup from "yup";
+
+export const schemaRegister = yup.object().shape({
+    username: yup.string().min(6, "Ingrese al menos 6 caracteres").required("Usuario requerido"),
+    password: yup.string().min(6, "Ingrese al menos 6 caracteres").required("Constraseña requerido."),
+    email: yup.string().email( "Debe ser un correo").required("Correo requerido.")
+  }).required();
