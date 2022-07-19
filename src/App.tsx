dca250f15dd7e2a321862345de964ea82558fa7a
jkcraftdom <jkcraftdom@gmail.com>
2022-07-19 16:50:12 -0500

    login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Navbar from './pages/Shared/navbar'
 import Navigation from './routes'
+import { getToken } from './services/auth'
 
 function App() {
-
 
   return (
     <BrowserRouter>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,17 +6,23 @@ import {
   Label,
   Button,
   MessageError,
-  Input  
+  Input,  
+  Error
 } from '../components/form'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { loginUser, formValues, setToken } from '../services/auth'
 import { useNavigate } from 'react-router-dom'
 import useTimer from '../hooks/useTimer'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { schemaLogin } from '../schemas/auth'
 
 const login = () => {
   const [isLoading, setLoading] = useState<boolean>(false)
   const {value: showMessage, initTimer} = useTimer(2)
-  const {register, handleSubmit} = useForm<formValues>()
+  const {register, handleSubmit, formState: {errors}} = useForm<formValues>({
+    mode: 'onBlur',
+    resolver: yupResolver(schemaLogin)
+  })
   let navigate = useNavigate()
 
   const onSubmit:SubmitHandler<formValues> =  async (data) => { 
@@ -47,8 +53,10 @@ const login = () => {
       <form action=""  style={styleForm} onSubmit={handleSubmit(onSubmit)}>
         <Label >Usuario</Label>
         <Input {...register('username')}/>
+        {errors.username && <Error>{errors.username.message}</Error>}
         <Label >Contraseña</Label>
         <Input type="password" {...register('password')}/>
+        {errors.password && <Error>{errors.password.message}</Error>}
         <Button >{isLoading? "Cargando ...":"Iniciar sesión"}</Button>
       </form>
     </Card>
