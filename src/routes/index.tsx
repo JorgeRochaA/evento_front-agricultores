f21b8f2fb1ef4dda01b8418c264ee7954af7ddbc
jkcraftdom <jkcraftdom@gmail.com>
2022-07-21 22:08:26 -0500

    page 404

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,19 @@ import Login from '../pages/login'
 import UserRegister from '../pages/user-register'
 import PanelUser from '../pages/user-panel'
 import PrivateRoute from '../hocs/privateRoute'
+import Pruebas from '../pages/pruebas'
+import Page404 from '../pages/page-404'
 
 const routes  = () => (    
     <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/pruebas" element={<Pruebas/>}/>
         <Route path="/register" element={<UserRegister/>}/>
         <Route path="/panel-user" element={
             <PrivateRoute><PanelUser/></PrivateRoute>            
         }/>
-        
+        <Route path="*" element={<Page404/>} />
     </Routes>
 )
 
