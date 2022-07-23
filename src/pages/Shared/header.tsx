7ee01d0327a44b9c13112e5c2bace45e7b27aca6
JorgeRochaA <jor2019.cr@gmail.com>
2022-07-23 17:15:40 -0600

    fix: fix responsive design from navbar

--- src/pages/Shared/header.tsx
+++ src/pages/Shared/header.tsx
@@ -1,63 +1,154 @@
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logo.png'
-import UserInfo from '../../components/user'
-import { useAppSelector } from '../../redux/hooks'
-import {selectUser} from '../../redux/slices/auth'
+import { Link } from "react-router-dom";
+import { selectUser } from "../../redux/slices/auth";
+import { useAppSelector } from "../../redux/hooks";
+import { useState } from "react";
+import logo from "../../assets/logo.png";
+import styled from "styled-components";
+import UserInfo from "../../components/user";
 
 const Container = styled.nav`
+  height: 70px;
   width: 100%;
-  height: 9vh;
-  color: var(--color2);
-  background-color: #F5F5F5;
-  border-bottom: 1px solid #cdcdcd;
-  padding: .5rem 1rem;
   display: flex;
-  flex-direction: row;
+  justify-content: space-evenly;
   align-items: center;
+  background-color: #f5f5f5;
+  color: var(--color2);
+  padding: 25px;
+  border-bottom: 1px solid #cdcdcd;
+`;
 
-`
+const Burger = styled.div`
+  height: 50px;
+  width: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-left: auto;
+  .bar {
+    height: 3px;
+    width: 70%;
+    background-color: #2c3c5b;
+    border-radius: 5px;
+    transition: 0.5s;
+  }
+  &.open {
+    .bar {
+      &:first-child {
+        transform: translate(0, 7px) rotate(-45deg);
+      }
+      &:last-child {
+        transform: translate(0, -11px) rotate(40deg);
+      }
+    }
+  }
+  @media screen and (min-width: 654px) {
+    display: none;
+  }
+`;
 const Logo = styled.img`
   height: 54px;
-`
+  margin-top: 6px;
+`;
 
 const ItemContainer = styled.div`
+  position: absolute;
+  top: 70px;
+  left: -100%;
+  height: 150px;
+  width: 100%;
   display: flex;
-  gap: 10px; 
-  flex-grow: 1;
-  justify-content: flex-end;   
-`
-const Item = styled.div`
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: #f5f5f5;
+  transition: 0.5s;
+  z-index: 2;
+  border-bottom: 1px solid #cdcdcd;
+  &.open {
+    left: 0;
+  }
+  & > div:not(:last-child) {
+    border-bottom: 1px solid #cdcdcd;
+  }
+  @media screen and (min-width: 655px) {
+    position: relative;
+    top: 0;
+    left: 0;
+    height: auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    background-color: #f5f5f5;
+    gap: 25px;
+    border: none;
+    & > div:not(:last-child) {
+      border-bottom: none;
+      width: auto;
+    }
+  }
+`;
+export const Item = styled.div`
   font-weight: bold;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 0.5em 0;
   & > a {
-    text-decoration:none;
+    text-decoration: none;
     color: inherit;
+    height: 100%;
   }
 
-`
+  @media screen and (min-width: 655px) {
+    max-width: fit-content;
+  }
+`;
 
 const header = () => {
-  const user = useAppSelector(selectUser)
+  const user = useAppSelector(selectUser);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
     <Container>
-        <Link to="/">
-            <Logo src={logo} alt="logo"/>
-        </Link>   
-          <ItemContainer>
-              <Item>Lista de mayorista</Item>
-              { user.token ?
-                <>
-                  <Item><Link to="/panel-user">Panel</Link></Item>
-                  <Item><UserInfo username={user?.username}/></Item>                                
-                </>
-                :<>
-                  <Item><Link to="/register">Registrarse</Link></Item>
-                  <Item><Link to="/login" className='link' >Iniciar sesión</Link></Item>                
-                </>
-              }              
-          </ItemContainer>
+      <Link to="/">
+        <Logo src={logo} alt="logo" />
+      </Link>
+      <Burger className={menuIsOpen ? "open" : ""} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </Burger>
+      <ItemContainer className={menuIsOpen ? "open" : ""}>
+        <Item>Lista de mayorista</Item>
+        {user.token ? (
+          <>
+            <Item>
+              <Link to="/panel-user">Panel</Link>
+            </Item>
+            <Item>
+              <UserInfo username={user?.username} toggleMenu={toggleMenu} />
+            </Item>
+          </>
+        ) : (
+          <>
+            <Item onClick={toggleMenu}>
+              <Link to="/register">Registrarse</Link>
+            </Item>
+            <Item onClick={toggleMenu}>
+              <Link to="/login" className="link">
+                Iniciar sesión
+              </Link>
+            </Item>
+          </>
+        )}
+      </ItemContainer>
     </Container>
-  )
-}
+  );
+};
 
-export default header
+export default header;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import styled from 'styled-components'
-import buyer from '../assets/buyer.jpg'
-import icon from '../assets/search.png'
-
+import React from "react";
+import styled from "styled-components";
+import buyer from "../assets/buyer.jpg";
+import icon from "../assets/search.png";
 
 const Container = styled.div`
   width: 100%;
   color: var(--color2);
-  height: 1vh;
-  
-`
+`;
 
 const Banner = styled.section`
   margin-top: 4px;
   padding: 3rem 1rem;
   display: flex;
   gap: 2rem;
-  background: rgb(21,31,53);
-  background: linear-gradient(90deg, rgba(21,31,53,1) 0%, rgba(44,60,91,1) 35%);
-`
+  background: rgb(21, 31, 53);
+  background: linear-gradient(
+    90deg,
+    rgba(21, 31, 53, 1) 0%,
+    rgba(44, 60, 91, 1) 35%
+  );
+`;
 
 const Contenido = styled.div`
   width: 50%;
-`
+`;
 
 const Title = styled.p`
   margin-bottom: 1rem;
@@ -30,66 +31,66 @@ const Title = styled.p`
   font-size: 3rem;
   line-height: 3rem;
   color: var(--color4);
-`
+`;
 
-const Image = styled.div`
-  
-`
+const Image = styled.div``;
 
 const BuyerImage = styled.img`
   width: 600px;
   display: block;
   border-radius: 10px;
-`
+`;
 
 const Paragraph = styled.p`
   font-size: 1.5rem;
   line-height: 2rem;
   color: Var(--color9);
-`
+`;
 const SearchContainer = styled.div`
   margin-top: 1rem;
   display: flex;
-`
+`;
 
 const Search = styled.input`
-    line-height: 2.5rem;
-    font-size: 1rem;
-    width: 17rem;
-    background: none;
-    border: 1px solid rgba(255,255,255,.4);
-    padding: 0 .5rem;
-    border-radius: .25rem 0 0 .25rem;
-    color: Var(--color9);
-`
+  line-height: 2.5rem;
+  font-size: 1rem;
+  width: 17rem;
+  background: none;
+  border: 1px solid rgba(255, 255, 255, 0.4);
+  padding: 0 0.5rem;
+  border-radius: 0.25rem 0 0 0.25rem;
+  color: Var(--color9);
+`;
 const Icon = styled.img`
-    padding: 0 1rem;
-    border-radius: 0 .25rem .25rem 0;
-    border: none;
-    cursor: pointer;
-    background-color: var(--color6);
-    object-fit: scale-down;
-`
+  padding: 0 1rem;
+  border-radius: 0 0.25rem 0.25rem 0;
+  border: none;
+  cursor: pointer;
+  background-color: var(--color6);
+  object-fit: scale-down;
+`;
 
 const page = () => {
   return (
     <Container>
-
       <Banner>
         <Contenido>
           <Title>Encuentre su cliente mayorista</Title>
-          <Paragraph>Proporcionamos a los agricultores información sobre cómo encontrar y vender sus productos, podemos ayudarlos a aumentar sus ingresos.</Paragraph>
+          <Paragraph>
+            Proporcionamos a los agricultores información sobre cómo encontrar y
+            vender sus productos, podemos ayudarlos a aumentar sus ingresos.
+          </Paragraph>
           <SearchContainer>
-            <Search type="text" placeholder='Buscar mayorista por producto' />
-            <Icon src={icon} alt="icon" />            
+            <Search type="text" placeholder="Buscar mayorista por producto" />
+            <Icon src={icon} alt="icon" />
           </SearchContainer>
         </Contenido>
         <Image>
-            <BuyerImage src={buyer} alt="buyer" />
+          <BuyerImage src={buyer} alt="buyer" />
         </Image>
       </Banner>
     </Container>
-  )
-}
+  );
+};
 
-export default page
+export default page;
