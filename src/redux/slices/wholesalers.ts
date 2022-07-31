dde72e8fb660dbea48418b027c2553fe69988257
jkcraftdom <jkcraftdom@gmail.com>
2022-07-31 18:24:29 -0500

    state wholesalers in redux

--- src/redux/slices/wholesalers.ts
+++ src/redux/slices/wholesalers.ts
@@ -1,59 +1,94 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
-import {formValues, filterWholesalers } from '../../services/wholesalers'
+import {formValues, filterWholesalers,wholesalerResponse } from '../../services/wholesalers'
 import {stateAsync} from '../../types'
+import _ from 'lodash'
 
-interface wholesaler {
-    username: string;
-    token: string
+interface options extends formValues{
+    isSortLocal: boolean;
 }
 
 interface wholesalersState {
-    wholesalers: wholesaler[];
-    status: stateAsync
+    wholesalers: wholesalerResponse[];
+    status: stateAsync;
+    options: options;
 }
 
 const initialState: wholesalersState = {
     wholesalers: [],
-    status: 'idle'
+    status: 'idle',
+    options: {itemPerPage: 10, sortBy: 'name', isSortLocal: false}
 }
 
-export const loginAsync = createAsyncThunk(
+const joinData = (data:formValues, state:RootState):formValues => {
+    const {sortBy, itemPerPage} = state.wholesalers.options
+    const dataNew = {...data, sortBy, itemPerPage}
+    return dataNew
+}
+
+const joinDataState = (state:RootState): formValues => {
+    const {country, itemPerPage, name, product, sector, sortBy} = state.wholesalers.options
+    const dataNew: formValues = {country, itemPerPage, name, product, sector, sortBy}
+    return dataNew
+}
+
+export const filterWholesalersAsync = createAsyncThunk(
     'wholesalers/filter', 
-    async (data: formValues) => {
-        const res = await filterWholesalers(data)
-        return res.data
+    async (data: formValues|null, {getState}) => {
+        const state = getState() as RootState
+        let dataNew = data? joinData(data, state) : joinDataState(state)
+        const res = await filterWholesalers(dataNew)        
+        return res
     }
 )
 
+
 export const wholesalers = createSlice({
-    name: 'auth',
+    name: 'wholesalers',
     initialState,
     reducers: {
-        setWholesalers: (state, action: PayloadAction<wholesaler[]>) => {
+        setWholesalers: (state, action: PayloadAction<wholesalerResponse[]>) => {
             state.wholesalers = action.payload
         },
         clearWholesalers: (state) => {
             state.wholesalers = []
+        }, 
+        setOptionSortBy: (state, action:PayloadAction<string>) => {
+            state.options.sortBy = action.payload
+            state.options.isSortLocal = true
+        },
+        setOptionItemPerPage: (state, action:PayloadAction<number>) => {
+            state.options.itemPerPage = action.payload
         }
     },
     extraReducers: (builder) => {
         builder
-            .addCase(loginAsync.pending, (state)=> {
-                state.status = 'loading'
+            .addCase(filterWholesalersAsync.pending, (state)=> {
+                state.status = "pending"
             })
-            .addCase(loginAsync.fulfilled, (state, action)=> {
-                state.status = 'idle'
+            .addCase(filterWholesalersAsync.fulfilled, (state, action)=> {
+                state.status = "succeeded"
+                state.options.isSortLocal = false
                 state.wholesalers = action.payload
             })
-            .addCase(loginAsync.rejected, (state)=> {
+            .addCase(filterWholesalersAsync.rejected, (state)=> {
                 state.status = 'failed'
             })
     }
 })
 
-export const {setWholesalers, clearWholesalers} = wholesalers.actions
+export const {setWholesalers, clearWholesalers, setOptionSortBy, setOptionItemPerPage} = wholesalers.actions
 
-export const selectUser = (state: RootState) => state.auth.user
+export const selectWholesaler = (state: RootState) => state.wholesalers.wholesalers
+
+export const selectIsSortLocal = (state: RootState) => state.wholesalers.options.isSortLocal
+
+export const selectSortWholesalers = (state: RootState) => {
+    const wholesalers = state.wholesalers.wholesalers
+    const sortBy = state.wholesalers.options.sortBy
+    return _.sortBy(wholesalers, [sortBy, 'asc']) as wholesalerResponse[]
+}
+
+export const selectCountWholesaler = (state: RootState) => state.wholesalers.wholesalers.length
 
 export default wholesalers.reducer
