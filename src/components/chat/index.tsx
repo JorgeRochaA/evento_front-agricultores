76aa56972f6d6152ac55a3651695f6c66d7463be
jkcraftdom <jkcraftdom@gmail.com>
2022-08-11 22:38:04 -0500

    container component

---
+++ src/components/chat/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    grid-area: chat;
+`
+
+const App = () => {
+  return (
+    <Container>Chat</Container>
+  )
+}
+
+export default App
