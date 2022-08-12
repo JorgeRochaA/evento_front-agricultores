76d1d9a455c2d08664c9d2a409ac29d9c286b7bd
jkcraftdom <jkcraftdom@gmail.com>
2022-08-11 22:39:54 -0500

    move components to wholesaler folder

---
+++ src/components/wholesaler/wholesaler-filter-by/index.tsx
@@ -0,0 +1,59 @@
+import { CardFilters, Input, Label, ButtonRed} from '../../common'
+import { styleContainterItemLabel } from '../../styles/index'
+import { useState } from "react";
+import { property } from '../../../types';
+
+export interface params {
+  title: string; 
+  placeholder: string;
+  name: string;
+  onChange?: (property:property)=>void;
+}
+
+function CardFilter(params: params) {
+
+  const [valueInput, setValueInput] = useState<string[]>([])
+  const [isEnter, setIsEnter] = useState<boolean>(false)
+
+  const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => setValueInput([event.target.value])
+
+  const handleEvent= (event:any) => {
+    if(event.key === 'Enter'){
+      setIsEnter(true)
+      if(typeof params.onChange === 'function') 
+        params.onChange({name:params.name, value: valueInput[0]})
+    }else{
+      setIsEnter(false)
+    }
+  }
+
+  const handleDelete= () => {
+    setIsEnter(false)
+    if(typeof params.onChange === 'function') 
+      params.onChange({name: params.name, value: ''})
+  }
+  
+  return (
+    <CardFilters>
+      <Label>{params.title}</Label>
+      <Input 
+        type="text"
+        placeholder={params.placeholder}
+        onChange={handleChange}
+        onKeyDown={handleEvent}
+      />
+      {
+        isEnter===true ? 
+          valueInput.map((val, index)=> (
+            <div key={index} style={styleContainterItemLabel}>
+              <Label>{val}</Label>
+              <ButtonRed onClick={handleDelete}>X</ButtonRed>
+            </div>
+          ))
+        : <></> 
+      }
+    </CardFilters>
+  )
+}
+
+export default CardFilter
