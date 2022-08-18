138cb4a39c9e5ee188aebca37e45636abd064001
JorgeRochaA <jor2019.cr@gmail.com>
2022-08-18 17:16:54 -0600

    feat: add chat component

--- src/pages/message-panel.tsx
+++ src/pages/message-panel.tsx
@@ -1,34 +1,32 @@
-import styled from 'styled-components'
-import Aside from './layout/aside'
-
+import styled from "styled-components";
+import Aside from "./layout/aside";
+import Chat from "../components/chat/index";
 const Container = styled.div`
   display: flex;
   width: 100%;
   height: 91vh;
-  justify-content: space-between;  
-  background-color:var(--color9);  
+  justify-content: space-between;
+  background-color: var(--color9);
   box-sizing: border-box;
   overflow-y: scroll;
-`
+`;
 
 const Main = styled.main`
   flex: 0 0 86%;
-  margin: 0 40px 0 160px;
+  margin-left: 125px;
   display: grid;
-  grid-template-areas: 
-    "chats messages"
-  ;
-`
+  grid-template-areas: "chats messages";
+`;
 
 const MessagePanel = () => {
-
   return (
     <Container>
-      <Aside/>
+      <Aside />
       <Main>
+        <Chat />
       </Main>
     </Container>
-  )
-}
+  );
+};
 
-export default MessagePanel
+export default MessagePanel;
