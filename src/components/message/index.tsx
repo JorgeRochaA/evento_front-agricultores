4a3608f051c7f65fdef7a275c38f4577ee1c19d4
JorgeRochaA <jor2019.cr@gmail.com>
2022-08-27 15:31:20 -0600

    feat: add pass params to MessageInput component

--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -1,18 +1,24 @@
-import styled from 'styled-components'
-import MessageInput from './message_input/messageInput'
-import Header from './message-header'
+import styled from "styled-components";
+import MessageInput from "./message_input/messageInput";
+import Header from "./message-header";
 
 const Container = styled.div`
-    flex: 0 0 50%;
-`
+  flex: 0 0 50%;
+`;
 
 const App = () => {
   return (
-    <Container>     
-      <Header name='Usuario'/>
-      <MessageInput/>
+    <Container>
+      <Header name="Usuario" />
+      <MessageInput
+        chat={1} //valor quemado
+        sender={"testJorge"} //valor quemado
+        onNewMessage={function onNewMessage() {
+          console.log("hay un nuevo mensaje");
+        }}
+      />
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
