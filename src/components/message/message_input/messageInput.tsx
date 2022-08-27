6c155c42c107803087c5f0c82071888ffe314337
JorgeRochaA <jor2019.cr@gmail.com>
2022-08-27 15:31:41 -0600

    feat: add logic to message input component

--- src/components/message/message_input/messageInput.tsx
+++ src/components/message/message_input/messageInput.tsx
@@ -1,30 +1,90 @@
-import {useState} from 'react';
-import {IoIosSend} from 'react-icons/io';
-import './messageInputStyles.css'
+import "./messageInputStyles.css";
+import { createMessage } from "../../../services/messages";
+import { formValues } from "../../../services/messages";
+import { IoIosSend } from "react-icons/io";
+import { MessageError } from "../../common/index";
+import { schemaMessage } from "../../../schemas/message";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+interface props {
+  chat: number;
+  onNewMessage: () => void;
+  sender: string;
+}
+const MessageInput = (props: props) => {
+  const [message, setMessage] = useState("");
 
-function MessageInput() {
+  const handleChange = (e: any): void => {
+    setMessage(e.target.value);
+  };
 
-    const [message,setMessage] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formValues>({
+    mode: "onBlur",
+    resolver: yupResolver(schemaMessage),
+  });
 
-    const handleChange = (e:any) => {
-        setMessage(e.target.value)
-    }
-
-    const handleSubmit = () => {
-        //LOGICA DE ENVIO
-        setMessage('')
-    }
+  const onSubmit: SubmitHandler<formValues> = () => {
+    let newMessage: formValues = {
+      chatRoomId: props.chat,
+      created_at: new Date().toISOString(),
+      emisor: props.sender,
+      textMessage: message,
+    };
+    createMessage(newMessage)
+      .then((result) => {
+        setMessage("");
+        props.onNewMessage();
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div className='input-container'>
-        <input type="text" 
-        placeholder='Mensaje...'
-        className='input' name="message" value={message} onChange={e => handleChange(e)}/>
-        <button type="button" className='btn' onClick={handleSubmit}>
-        <IoIosSend/>
+    <div
+      className="input-container"
+      style={{
+        flexDirection: "column",
+        position: "fixed",
+        bottom: "10px",
+        width: "45%",
+      }}
+    >
+      <MessageError
+        style={{
+          height: "20px",
+          width: "260px",
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+          fontSize: "12px",
+          backgroundColor: "transparent",
+          color: "red",
+        }}
+      >
+        {errors.textMessage && errors.textMessage.message}
+      </MessageError>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Mensaje..."
+          className="input"
+          value={message}
+          {...register("textMessage")}
+          onChange={(e) => handleChange(e)}
+        />
+        <button type="submit" className="btn">
+          <IoIosSend />
         </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default MessageInput
+export default MessageInput;
